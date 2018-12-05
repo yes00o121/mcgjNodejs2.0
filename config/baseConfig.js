0cab1e173de37d0d60477769db7d1de49467fd86
yes00o121 <825670179@qq.com>
2018-12-05 09:17:08 +0800

    提交配置文件

--- config/baseConfig.js
+++ config/baseConfig.js
@@ -2,7 +2,7 @@
 ﻿export default {
   port : 8090,//服务器端口
   localhost : 'http://127.0.0.1:8090/mcgj/',//服务器地址
-  websocket : 'ws://127.0.0.1:8090/websocket',//websocket地址
+  websocket : 'ws://127.0.0.1:8090/mcgj/websocket',//websocket地址
   imgUrl : '/common/image',//获取图片地址
   defaultErrorMessage : '服务宕机啦,请稍后再试'
 }
